19e7bd67745cd752433bfa5d19bbd38348f7bde1
Divyam Kumar <divyam.kumar@sap.com>
2020-07-11 10:18:00 +0530

    Few starting lectors

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python
-Learning python
+`Learning python`
+
+
---
+++ main.py
@@ -0,0 +1,26 @@
+import flask
+# This is a comment
+
+"""This is a
+multi
+line 
+comment"""
+
+# Following code with harry tutorial
+
+print("Hello", end=" ")
+print("Hello")
+print("Divyam is \n good boy \t1")
+
+test = "name"
+test1 = "to MyName"
+print(test+test1)
+myNum = 23
+myfloat = 23.3
+print(myNum+myfloat)
+# print(myNum+test)  TypeError cannot add int with dtr
+print(type(myNum))
+print(type(test))
+print(type(myfloat))
+
+###########################################
