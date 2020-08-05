1fb98db0fc3e3b5a7c53e8cdb518e05a37b24d58
Divyam Kumar <divyamkumarsingh@gmail.com>
2020-08-05 13:39:08 +0530

    faulty calculator

---
+++ FaultyCalculator.py
@@ -0,0 +1,19 @@
+#   This is a faulty calculator
+
+#   45*3 = 555 , 56 + 9 = 77, 56/6 = 4
+
+print("Chose the operation which you want to perform")
+print(" 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division")
+operator = int(input("\n"))
+print("\n Enter first number")
+num1 = int(input("\n"))
+print("\n Enter second number")
+num2 = int(input("\n"))
+if operator == 1:
+    print("Sum is ", (num1+num2))
+elif operator == 2:
+    print("Subtraction is ", num1-num2)
+elif operator == 3:
+    print("Multiplication is ", num1*num2)
+elif operator == 4:
+    print("Division is ", num1/num2)
