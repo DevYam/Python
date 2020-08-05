24eadc08320edd072a1612b50fa544b6057822b6
Divyam Kumar <divyamkumarsingh@gmail.com>
2020-08-05 15:28:01 +0530

    Faulty calculator

--- FaultyCalculator.py
+++ FaultyCalculator.py
@@ -9,11 +9,18 @@ print("\n Enter first number")
 num1 = int(input("\n"))
 print("\n Enter second number")
 num2 = int(input("\n"))
-if operator == 1:
-    print("Sum is ", (num1+num2))
+
+if operator == 1 and num1 == 56 and num2 == 9:
+    print("sum is ", 77)
+elif operator == 3 and num1 == 45 and num2 == 3:
+    print("Multiplication is ", 555)
+elif operator == 4 and num1 == 56 and num2 == 6:
+    print("Division is ", 4)
+elif operator == 1:
+    print("Sum is ", (num1 + num2))
 elif operator == 2:
-    print("Subtraction is ", num1-num2)
+    print("Subtraction is ", num1 - num2)
 elif operator == 3:
-    print("Multiplication is ", num1*num2)
+    print("Multiplication is ", num1 * num2)
 elif operator == 4:
-    print("Division is ", num1/num2)
+    print("Division is ", num1 / num2)
