6d3094855430db4a65216c618415d8b520d8b0ae
Divyam Kumar <divyamkumarsingh@gmail.com>
2020-08-07 11:30:15 +0530

    shorthand if else

---
+++ shorthandifelse.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number a\n"))
+b = int(input("Enter second number b\n"))
+
+#   ShortHand if and shorthand if else
+
+# if a > b: print("a is greater than b")
+
+print("a is greater than b") if a > b else print(" a is less than b")
