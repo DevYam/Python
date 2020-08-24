1703f7e9e6cc46ac13219dfdc035d92d91cb432c
Divyam Kumar <divyamkumarsingh@gmail.com>
2020-08-24 10:21:11 +0530

    added ex4 Star Printing program

--- ex4.py
+++ ex4.py
@@ -2,16 +2,16 @@
 #   also take a boolean from user to print upright or inverted * triangle
 
 rows = int(input("Enter the number of rows wanted in the triangle * pattern\n"))
-orientation = bool(input("Enter 0 for inverted and 1 for upright triangle\n"))
+orientation = int(input("Enter 0 for inverted and 1 for upright triangle\n"))
 
 if orientation == 1:
-    for i in range(rows):
-        print(" ")
-        for j in range(i):
-            print("* ", end="")
+   for i in range(rows):
+    for j in range(i):
+         print("*", end=" ")
+    print()
 
-# if orientation == 0:
-#     for i in range(rows):
-#         print("* ")
-#         for j in range(i):
-#             print(" ", end="")
+if orientation == 0:
+    for i in reversed(range(rows)):
+        for j in range(i):
+            print("*", end=" ")
+        print()
